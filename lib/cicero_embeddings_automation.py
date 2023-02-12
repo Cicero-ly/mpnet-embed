4259edc995f030ba29bb35c94afa6461f074fec6
Tyler Termini <69917602+waycroft@users.noreply.github.com>
2023-02-11 16:34:32 -0800

    successfully embedding with banana all in one batch, then storing vectors in pinecone with foreign key referencing the original mongodb doc.

--- lib/cicero_embeddings_automation.py
+++ lib/cicero_embeddings_automation.py
@@ -5,7 +5,7 @@ import banana_dev as banana
 import pprint
 import sys
 import pinecone
-from nanoid import generate
+from nanoid import generate as generate_nanoid
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -28,7 +28,7 @@ class Embed:
         db_connection_string = os.environ["MONGO_CONNECTION_STRING"]
         client = MongoClient(db_connection_string)
         db = client["cicero_thoughts"]
-        self.news = db["news"]
+        self.news = db["news_test_embed"]
         print("MongoDB connection successful")
 
     def __init__(self):
@@ -41,12 +41,16 @@ class Embed:
         return thought["title"] + " " + soup.get_text(strip=True)
 
     def start_job(self, max_size):
+        # TODO: immediately return 200 response to client and start job in background, since it will take a while
+        # TODO: add job status to mongodb
+        # Should be able to poll job status
         status = "Success"
         i = 0
         thoughts_to_encode = []
 
         for thought in self.news.find(
             {
+                "mpnet_embedding_pinecone_key": {"$exists": False},
                 "valuable": True,
                 "reviewed": True,
                 "title": {"$ne": None},
@@ -58,46 +62,69 @@ class Embed:
             serialized_thought = self.serialize_thought_for_model(thought)
             thoughts_to_encode.append(("news", thought["_id"], serialized_thought))
 
+        print(
+            "Thoughts serialized successfully. Number of thoughts to embed: ",
+            len(thoughts_to_encode),
+        )
+
         try:
             prompts = [x[2] for x in thoughts_to_encode]
-            pp.pprint(prompts)
 
-            # thoughts are aggregated and send to banana all at once
+            # Thoughts are aggregated and send to banana all at once
             # to minimize the time the workload is up (would rather it not be idle between API calls)
-            # It's cheaper to run this job than run banana longer
+            # It's cheaper to run this job longer than run banana longer
+            print("Sending thoughts to banana for embedding...")
             banana_output = banana.run(
                 self.banana["api_key"], self.banana["model_key"], {"prompt": prompts}
             )
             vectors = banana_output["modelOutputs"][0]["data"]
 
-            # print("banana output:")
-            # pp.pprint(vectors)
+            print("sample of banana output: ")
+            for vector in vectors:
+                pp.pprint(vector[:3])
 
             assert len(thoughts_to_encode) == len(vectors)
 
+            print("All embeddings received from banana. Uploading to pinecone...")
+
             for i, element in enumerate(thoughts_to_encode):
-                embedding_key = generate()
+                embedding_key = generate_nanoid()
                 # TODO: we'll want to future proof this for other thought collections
                 # besides just news
-                # thought_collection = element[0]
+                thought_collection = element[0]
                 thought_id = element[1]
                 thought_vector = vectors[i]
 
-                self.pinecone_index.upsert(embedding_key, thought_vector)
+                print("Uploading embedding for thought ", thought_id, " to pinecone...")
+                print("sample of thought_vector: ")
+                pp.pprint(thought_vector[:3])
+                self.pinecone_index.upsert(
+                    [
+                        (
+                            embedding_key,
+                            thought_vector,
+                            {
+                                "thought_collection": thought_collection,
+                            },
+                        )
+                    ]
+                )
 
+                print(
+                    "Updating embedding key for thought ", thought_id, " in mongodb..."
+                )
                 self.news.update_one(
                     {"_id": thought_id},
                     {"$set": {"mpnet_embedding_pinecone_key": embedding_key}},
                 )
+                print("Done")
                 i += 1
 
         except Exception as e:
             print("Error: ", e)
             status = "Error: " + str(e)
 
-        # pp.pprint(thoughts_to_encode)
-        # print("size in memory of thoughts to encode: ", sys.getsizeof(thoughts_to_encode))
-
+        print("Job completed with status: ", status, " and ", i, " thoughts embedded.")
         return status, i
 
 
