8f0d20005331b66a17bf4395432dc0112e22cc20
Tyler Termini <69917602+waycroft@users.noreply.github.com>
2023-02-12 19:11:45 -0800

    tests outline

---
+++ lib/test.py
@@ -0,0 +1,12 @@
+# TESTS
+
+# Test 1:
+# Make sure the thoughts collected initially maintain the same sort order,
+# such that the return vectors can always be guaranteed to match.
+
+# Test 2:
+# Make sure max_size is always respected
+
+# Test 3:
+# Make sure "thoughts_encoded" in the job document written to DB
+# is only populated once job is successfully completed.
