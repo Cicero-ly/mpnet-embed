ebd47c52848fc63b3c78ae2d226f9efc8979bc3b
Tyler Termini <69917602+waycroft@users.noreply.github.com>
2023-02-20 18:44:19 -0800

    more detailed cron job output, and proper exit codes

--- cron/cron.py
+++ cron/cron.py
@@ -1,15 +1,49 @@
 import requests
 import os
+import sys
+import time
 
 
-def make_request():
-    endpoint_url = os.environ["EMBED_JOB_ENDPOINT"]
-    url = f"{endpoint_url}/run-embed"
-    data = {"max_size": 5000}
-    response = requests.post(url, json=data)
-    return response
+class Job:
+    def __init__(self):
+        endpoint_url = os.environ["EMBED_JOB_ENDPOINT"]
+        self.job_start_url = f"{endpoint_url}/run-embed"
+        self.job_status_url = f"{endpoint_url}/get-job-status"
+    
+    def start_job(self):
+        print("Starting job...")
+        response = requests.post(self.job_start_url, json={"max_size": 2})
+        print(response.text)
+        if response.status_code != 200:
+            return response
+        return self.poll_job_status(response)
+    
+    def poll_job_status(self, response):
+        print("Job started/resumed successfully.")
+        job_id = response.json()["job_id"]
+        data = {"id": job_id}
+        for i in range(12):
+            print(f"Polling job status ({i}/12)...")
+            response = requests.get(self.job_status_url, params=data)
+            if response.status_code != 200:
+                return response
+            status = response.json()["status"]
+            if "error" in status.lower():
+                print("Job failed with status: " + status)
+                response.status_code = 500
+                return response
+            if "success" in status.lower():
+                print("Job complete with status: " + status)
+                return response
+            time.sleep(10)
+        return response
+
 
 if __name__ == "__main__":
-    response = make_request()
-    print(response.status_code)
-    print(response.text)
+    job = Job()
+    response = job.start_job()
+
+    if response.status_code != 200:
+        sys.exit(1)
+    else:
+        sys.exit(0)
